dff7f9e332f9d440cb99f97ca4b23a970e37747d
Guireg Naël <guireg.nael@ynov.com>
2026-06-09 15:02:09 +0200

    added the database

--- server.js
+++ server.js
@@ -1,5 +1,23 @@
 const handler = require("serve-handler");
 const http = require("http");
+const sql = require("mssql");
+
+const dbConfig = {
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  server: "db-guireg.database.windows.net",
+  database: "db-guireg",
+  options: { encrypt: true, trustServerCertificate: false },
+};
+
+sql
+  .connect(dbConfig)
+  .then(() => {
+    console.log("Connecté à la base de données SQL");
+  })
+  .catch((err) => {
+    console.error("Erreur de connexion SQL :", err);
+  });
 
 const server = http.createServer((request, response) => {
   return handler(request, response, { public: "." });
